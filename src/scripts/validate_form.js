a1c4093073cb76297112f8ab7ff188a07e79a53a
SaulGoofman <77206907+SaulGoofman@users.noreply.github.com>
2022-09-17 04:01:20 +0530

    changes to functionality

---
+++ src/scripts/validate_form.js
@@ -0,0 +1,15 @@
+const pass = document.getElementById("password").value;
+const confPass = document.getElementById("confpass").value;
+const error = document.getElementById("form-error");
+if (pass < 8) {
+    error.innerText = "Password must have a minimum length of 8 character";
+    return;
+}
+if (pass > 16) {
+    error.innerText = "Password can have a maximum length of 16 character";
+    return;
+}
+if (pass !== confPass) {
+    error.innerText = "Password and Confirm Password don't match";
+    return;
+}
